7d1306532e41440483961e966ecac8c8cfa096b9
uagalaxy <74304156+uagalaxy@users.noreply.github.com>
2026-06-09 15:30:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -907,10 +907,10 @@ downloadPdfBtn.onclick = () => {
         doc.text("uRhythm.vercel.app", pageWidth / 2, pageHeight / 2, { align: "center", angle: 45 });
 doc.setFontSize(30);
         doc.setTextColor(243, 243, 246); 
-        doc.text("uRhythm.vercel.app", pageWidth / 2, pageHeight / 2.4, { align: "center", angle: 45 });
+        doc.text("uRhythm.vercel.app", pageWidth / 2, pageHeight / 2.8, { align: "center", angle: 45 });
 doc.setFontSize(30);
         doc.setTextColor(243, 243, 246); 
-        doc.text("uRhythm.vercel.app", pageWidth / 2, pageHeight / 2.8, { align: "center", angle: 45 });
+        doc.text("uRhythm.vercel.app", pageWidth / 2, pageHeight / 3.4, { align: "center", angle: 45 });
 
         // 2. Clear Footer Identification & Hyperlink Watermark
         doc.setFontSize(8.5);
